8d3c5a265a24d01c77961bf0ad17adfd3a0e152b
Skyler Krouse <65509213+smk362@users.noreply.github.com>
2023-09-08 12:32:07 -0400

    added a word cloud for skills

--- portfolio/src/sections/Skills.js
+++ portfolio/src/sections/Skills.js
@@ -1,10 +1,42 @@
+import ReactWordcloud from 'react-wordcloud';
+
 export default function Home() {
+
+  const options = {
+    rotations: 0
+  }
+
+  const skills = [
+    {
+      text: 'Java',
+      value: 120
+    },
+    {
+      text: 'Python',
+      value: 60
+    },
+    {
+      text: "C++",
+      value: 40
+    },
+    {
+      text: "JavaScript",
+      value: 80
+    },
+    {
+      text: "TypeScript",
+      value: 80
+    }
+  ]
+
   return (
     <div id="skills" className="section">
       <div className="header">
         <h1>Skills</h1>
       </div>
-      <div></div>
+      <div>
+        <ReactWordcloud words={skills} options={options}></ReactWordcloud>
+      </div>
     </div>
   );
 }
