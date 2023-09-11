f3b5e3b9e333e6470aca9f9492805cd67d45e380
Skyler Krouse <65509213+smk362@users.noreply.github.com>
2023-09-11 14:43:45 -0400

    Added fun styling and fun contact info set up

--- portfolio/src/sections/Home.js
+++ portfolio/src/sections/Home.js
@@ -1,21 +1,47 @@
 import '../style.css'
-import Image from 'react'
+import { useState } from 'react'
 import grad_profile from '../assets/images/grad_profile.jpg'
 import profile from '../assets/images/profile.png'
 
 export default function Home() {
+
+  const [isHover, setIsHover] = useState(false);
+  const [contactInfo, setContactInfo] = useState("skyler2601@gmail.com");
+
+  const handleMouseEnter = (contactInfoSet) => {
+    setIsHover(true);
+    setContactInfo(contactInfoSet);
+
+  };
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  const contactStyle = {
+    display: isHover ? 'block' : 'none',
+  };
+
   return (
     <div id="home" className="section">
       <div className="header">
         <div>
           <h1>Skyler Krouse</h1>
-          <a href={"mailto:skyler2601@gmail.com"}>skyler2601@gmail.com</a>
-          <p>908-399-6601</p>
-          <a href={"https://www.linkedin.com/in/skyler-krouse/"} target="_blank">LinkedIn</a>
+          <div>
+            <div className="horizontal-flex">
+              {/* <a href={"mailto:skyler2601@gmail.com"}>skyler2601@gmail.com</a>
+              <p>908-399-6601</p>
+              <a href={"https://www.linkedin.com/in/skyler-krouse/"} target="_blank">LinkedIn</a> */}
+              <a href={"mailto:skyler2601@gmail.com"} className="contact" onMouseEnter={() => handleMouseEnter("skyler2601@gmail.com")} onMouseLeave={handleMouseLeave}>E-Mail</a>
+              <span className="contact" onMouseEnter={() => handleMouseEnter("(908) 399-6601")} onMouseLeave={handleMouseLeave}>Phone</span>
+              <a href={"https://www.linkedin.com/in/skyler-krouse/"} className="contact" onMouseEnter={() => handleMouseEnter("https://www.linkedin.com/in/skyler-krouse/")} onMouseLeave={handleMouseLeave}>LinkedIn</a>
+            </div>
+            <div style={{ height: '50px' }}><span style={contactStyle}>{contactInfo}</span></div>
+          </div>
+
         </div>
       </div>
       <div className="content">
-        <img src={profile} alt="Profile Picture" style={{ width: 400, borderRadius: 400 / 2 }}></img>
+        <img src={profile} alt="Profile Picture" style={{ width: "65%", borderRadius: 800 }}></img>
       </div>
     </div >
   );
